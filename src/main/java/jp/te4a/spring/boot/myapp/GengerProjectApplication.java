6081dfb8fc7e584865289d84498466c9b149bbee
150082 <150082@e0601-07.JC-21.local>
2018-06-19 12:59:37 +0900

    newcreate

---
+++ src/main/java/jp/te4a/spring/boot/myapp/GengerProjectApplication.java
@@ -0,0 +1,12 @@
+package jp.te4a.spring.boot.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GengerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GengerProjectApplication.class, args);
+	}
+}
