a78490348a7716b00b5f9639eb9b038c1ed0bb63
150293 <150293@e0601-08.JC-21.local>
2018-06-27 16:04:53 +0900

    Commit

--- src/test/java/jp/te4a/spring/boot/myapp/GengarProjectApplicationTests.java
+++ src/test/java/jp/te4a/spring/boot/myapp/GengarProjectApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class StudentApplicationTests {
+public class GengarProjectApplicationTests {
 
 	@Test
 	public void contextLoads() {
