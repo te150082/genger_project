97901c399cee5c75072402644fcbcb42cdaecbb7
140170 <140170@e0601-05.JC-21.local>
2018-06-28 10:52:24 +0900

    画面遷移

--- src/main/java/jp/te4a/spring/boot/myapp/LectureController.java
+++ src/main/java/jp/te4a/spring/boot/myapp/LectureController.java
@@ -1,11 +1,8 @@
 package jp.te4a.spring.boot.myapp;
-import java.util.List;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,37 +12,34 @@ import org.springframework.web.servlet.ModelAndView;
 public class LectureController {
     @Autowired
     LectureService lectureService;
-    @RequestMapping("lecturers/create")
+    @RequestMapping("lectures")
     public String index(Model model) {
         model.addAttribute("msg", "this is setting message");
-        return "lecturers/add-lec";
+        return "lectures/add-lec";
     }
-    @RequestMapping(value="lecturers/add-lec", method=RequestMethod.POST)
+    @RequestMapping(value="lectures/add-lec", method=RequestMethod.POST)
      public ModelAndView postForm(@RequestParam("name") String name, 
     @RequestParam("phone") String phone,@RequestParam("address") String address) {
-         ModelAndView mv = new ModelAndView("lecturers/add-lec");
+         ModelAndView mv = new ModelAndView("lectures/add-lec");
          lectureService.save(new LectureBean(name, phone, address));
          // TODO:DEBUG
          //List<LectureBean> a = new List();
          // mv.addObject("lectures", a);
-         mv.addObject("lecturers", lectureService.findAll());
+         mv.addObject("lectures", lectureService.findAll());
          return mv;
     }
-    @RequestMapping(value="lecturers/confirm-lec", method=RequestMethod.POST)
+    @RequestMapping(value="lectures/confirm-lec", method=RequestMethod.POST)
     public ModelAndView postForm2(@RequestParam("name") String name, 
-    	    @RequestParam("phone") String phone,@RequestParam("address") String address, @RequestParam("subject") String[] subject, LectureForm form) {
-    	    
+    	    @RequestParam("phone") String phone,@RequestParam("address") String address, LectureForm form) {
     	         form.setName(name);
     	         form.setPhone(phone);
     	         form.setAddress(address);
-    	         form.setSubject(subject);
-        
-    	         ModelAndView mv = new ModelAndView("lecturers/confirm-lec");
+    	         ModelAndView mv = new ModelAndView("lectures/confirm-lec");
     	         lectureService.save(new LectureBean(name, phone, address));
     	         // TODO:DEBUG
     	         //List<LectureBean> a = new List();
     	         // mv.addObject("lectures", a);
-    	         mv.addObject("lecturers", lectureService.findAll());
+    	         mv.addObject("lectures", lectureService.findAll());
     	         return mv;
     	    }
     
